68387c77e116e295e81149072d56f33cb311bcdf
jmsoper <jmsoper@gmail.com>
2018-12-25 17:07:46 -0800

    Further factor out write functionality

--- database.c
+++ database.c
@@ -10,34 +10,31 @@ void writeEntry(struct Student * student, FILE *file){
   fprintf(file, "%s %d\n", student->name, student->score);
 }
 
-int main(int argc, char *argv[]){
+void collectEntries(struct Student * student, char * filename){
+  char answer[1];
   FILE *pFile;
+
+  pFile = fopen(filename, "a");
+  while(strcmp(answer, "n")!=0){
+    printf("Name: ");
+    scanf("%s", student->name);
+    printf("Score: ");
+    scanf("%d", &student->score);
+    writeEntry(student, pFile);
+    printf("Would you like to add another student? [y/n]?");
+    scanf("%s", answer);
+  }
+  fclose(pFile);
+}
+
+int main(int argc, char *argv[]){
+  char *filename="data.txt";
   char name[21];
   int score;
   char answer[1];
   struct Student student;
 
-  // Open the file 
-  pFile = fopen("database.txt", "a");
-
-  if (pFile != NULL){
-    while(strcmp(answer, "n")!=0){
-      printf("Name: ");
-      scanf("%s", name);
-      printf("Test score: ");
-      scanf("%d", &score);
-      strcpy(student.name, name);
-      student.score = score;
-
-      //Store this information in the database
-      writeEntry(&student, pFile); 
-      printf("Are there any more students [y/n]?");
-      scanf("%s", answer);
-    }
-    fclose(pFile);
-  } else {
-    printf("Could not open the file.\n");
-  }
+  collectEntries(&student, filename);
 
   return 0;
 }
