e3e1d462d867de658d9bf2f69398098b21ca7080
jmsoper <jmsoper@gmail.com>
2018-12-25 17:51:09 -0800

    Allow user to give different commands for read/write/help

--- README.md
+++ README.md
@@ -4,11 +4,11 @@ A tiny toy database in C
 ![duck swimming in gold coins](https://media.giphy.com/media/gQdejV5BBChHi/giphy.gif)
 
 - [x] User can write entries to database
-- [ ] User can read entries from database
+- [x] User can read entries from database
 - [ ] User can find and read a specific entry from database
 - [ ] User can find and delete entries from database
 - [ ] User can find and edit entries from a database
-- [ ] Entries in database can have multiple fields as defined in a schema
-- [ ] Entries can be parsed into data objects
+- [x] Entries in database can have multiple fields as defined in a schema
+- [x] Entries can be parsed into data objects
 - [ ] User can fetch multiple entries from the database based on matching specific criteria
 
--- database.c
+++ database.c
@@ -27,14 +27,44 @@ void collectEntries(struct Student * student, char * filename){
   fclose(pFile);
 }
 
+int readEntry(struct Student * student, FILE *file){
+  if(fscanf(file, "%s %d\n", student->name, &student->score)==EOF){
+    return 0;
+  } else {
+    return 1;
+  } 
+}
+
+void readAllEntries(struct Student * student, char * filename){
+  FILE *pFile;
+  pFile = fopen(filename, "r");  
+
+  while (readEntry(student, pFile)){
+    printf("%s %d\n", student->name, student->score);
+  }
+}
+
 int main(int argc, char *argv[]){
   char *filename="data.txt";
-  char name[21];
-  int score;
-  char answer[1];
   struct Student student;
+  char command[10];
+  char * help="To add a student or students, type in 'add.' To read entries, type in 'read'. To end the program, type in 'exit'.\n";
 
-  collectEntries(&student, filename);
+  printf("%s", help);
+  do {
+    scanf("%s", command);
+    if (strcmp(command, "read")==0){
+      readAllEntries(&student, filename);
+    } else if (strcmp(command, "add")==0){
+      collectEntries(&student, filename);
+    } else if (strcmp(command, "exit")==0){
+      printf("Exiting program.\n");
+    } else if (strcmp(command, "help")==0){ 
+      printf("%s", help);
+    }else {
+      printf("'%s' is not an available command. Type in 'help' to view available commands.\n", command);
+    }
+  } while(strcmp(command,"exit")!=0);
 
   return 0;
 }
